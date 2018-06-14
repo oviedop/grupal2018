02c29b316de7fe027cb5abcfb3f5f97c6ef8f2e0
Diego Monge-Villegas <diegomongev@gmail.com>
2018-06-13 19:23:58 -0600

    resultados sobre probabilidades de presencia, von bertalanffy, y matriz, falta incluir la mortalidad

--- scriptgrupal.R
+++ scriptgrupal.R
@@ -1,11 +1,14 @@
 #ecuacion de von bertalanffy
 
-L<-Li*(1-e^-k(t-t0))
-k<-0.3706
   
 VB<-function(Li,k,t,t0){
-  Li<-
-  k<-0.3706
-  t<-
-  t0<-  
+  VB<-Li*(1-exp(-k*(t-t0)))
+  return(VB)
 }
+VB(Li=183.2, k=0.3706, t=2, t0=0.2)
+
+curve(expr =VB(
+      Li=183.2,
+      k=0.3706,
+      t0=-0.2,
+      t=x),from = 0, to=15)
